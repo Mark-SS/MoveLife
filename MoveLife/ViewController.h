41a0da640c25e53d1125957f80ae3201fc4e2a8b
gongliang <gongliang@yongche.com>
2013-09-05 15:17:56 +0800

    add moveLife

---
+++ MoveLife/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MoveLife
+//
+//  Created by gongliang on 13-9-5.
+//  Copyright (c) 2013年 gongliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MoveLifeTests/MoveLifeTests.h
@@ -0,0 +1,13 @@
+//
+//  MoveLifeTests.h
+//  MoveLifeTests
+//
+//  Created by gongliang on 13-9-5.
+//  Copyright (c) 2013年 gongliang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MoveLifeTests : SenTestCase
+
+@end
